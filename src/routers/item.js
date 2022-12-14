2208aaa2e1692cd83610124c7edbb7c62e8dab39
Midlaj13 <muhammedmidlaju@gmail.com>
2022-12-14 17:01:26 +0530

    first commit

---
+++ src/routers/item.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const Item = require('../models/item')
+const Auth = require('../middleware/auth')
+const router = new express.Router()
+
+
+//createanitem//
+
+router.post('/items',Auth, async(req, res) => {
+    try {
+    const newItem = new Item({
+        ...req.body,
+        owner: req.user._id
+    })
+       await newItem.save()
+       res.status(201).send(newItem)
+    } catch (error) {
+    res.status(400).send({message: "error"})
+    }
+    })
+
+//createanitem//
+
+
+//fetchanitem//
+
+router.get('/items/:id', async(req, res) => {
+    try{
+       const item = await Item.findOne({_id: req.params.id})
+    if(!item) {
+       res.status(404).send({error: "Item not found"})
+    }
+       res.status(200).send(item)
+    } catch (error) {
+       res.status(400).send(error)
+    }
+    })
+
+//fetchanitem//
+
+//fetchallitems//
+router.get('/items/:id', async(req, res) => {
+    try{
+       const item = await Item.findOne({_id: req.params.id})
+    if(!item) {
+       res.status(404).send({error: "Item not found"})
+    }
+       res.status(200).send(item)
+    } catch (error) {
+       res.status(400).send(error)
+    }
+    })
+//fetchalliems//
+
+
+//updateanitem//
+router.patch('/items/:id', Auth, async(req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'description', 'category', 'price']
+    const isValidOperation = updates.every((update) =>              allowedUpdates.includes(update))
+       if(!isValidOperation) {
+         return res.status(400).send({ error: 'invalid updates'})
+    }
+    try {
+      const item = await Item.findOne({ _id: req.params.id})
+      if(!item){
+          return res.status(404).send()
+      }
+      updates.forEach((update) => item[update] = req.body[update])
+      await item.save()
+      res.send(item)
+    } catch (error) {
+    res.status(400).send(error)
+    }
+    })
+//updateanitem//
+
+//deleteanitem//
+router.delete('/items/:id', Auth, async(req, res) => {
+    try {
+    const deletedItem = await Item.findOneAndDelete( {_id: req.params.id} )
+       if(!deletedItem) {
+        res.status(404).send({error: "Item not found"})
+    }
+       res.send(deletedItem)
+    } catch (error) {
+       res.status(400).send(error)
+    }
+    })
+
+//deleteanitem
+
+
+
+//export//
+
+module.exports=router
+
